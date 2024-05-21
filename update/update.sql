8c822f988f99decbd22344bd6350d63ebd958caf
paul nkale <paulnkale29@gmail.com>
2024-05-20 23:28:54 -0700

    updatedb

---
+++ update/update.sql
@@ -0,0 +1,9 @@
+USE restaurant;
+UPDATE customer SET cuctomername='nkale' ,
+customerphoneNo='0756645332' ,
+customeremail='nkale111@gmail.com',
+customeraddress='P.O.BOX 712 arusha' WHERE customerID='5';
+
+UPDATE dishes SET dishname='bryan_beef', 
+dishprice='50000',
+dishdescription='one_plate' WHERE dishiID='1';
